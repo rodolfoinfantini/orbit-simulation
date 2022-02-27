e7a78ce00c35dfe1d15bb30fc7a01bdd31f3e8bf
SirVoRaK <sirvoraky@gmail.com>
2022-02-27 17:44:09 -0300

    refactor

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const canvas = document.createElement('canvas')
 canvas.width = window.innerWidth
 canvas.height = window.innerHeight
 document.body.appendChild(canvas)
+const ctx = canvas.getContext('2d')
 
 const resetButton = document.querySelector('form p')
 
@@ -19,34 +20,53 @@ urlParams.forEach((value, key) => {
     document.querySelector(`#${key}`).value = value
 })
 
-const ctx = canvas.getContext('2d')
-
 const balls = []
 
-const ball1 = {
-    x: (canvas.width / 5) * 2,
-    y: canvas.height / 2,
-    vx: +urlParams.get('b1vx') || -2,
-    vy: +urlParams.get('b1vy') || 5,
-    mass: +urlParams.get('b1mass') || 60,
+const config = {
+    ball1: {
+        x: (canvas.width / 5) * 2,
+        y: canvas.height / 2,
+        vx: +urlParams.get('b1vx') || -2,
+        vy: +urlParams.get('b1vy') || 5,
+        mass: +urlParams.get('b1mass') || 60,
+    },
+    ball2: {
+        x: (canvas.width / 5) * 3,
+        y: canvas.height / 2,
+        vx: +urlParams.get('b2vx') || 2,
+        vy: +urlParams.get('b2vy') || -5,
+        mass: +urlParams.get('b2mass') || 50,
+    },
 }
-const ball2 = {
-    x: (canvas.width / 5) * 3,
-    y: canvas.height / 2,
-    vx: +urlParams.get('b2vx') || 2,
-    vy: +urlParams.get('b2vy') || -5,
-    mass: +urlParams.get('b2mass') || 50,
-}
+
 const constant = +urlParams.get('const') || null
-balls.push(new Ball(ball1.x, ball1.y, ball1.vx, ball1.vy, ball1.mass, null, constant))
-balls.push(new Ball(ball2.x, ball2.y, ball2.vx, ball2.vy, ball2.mass, balls[0], constant))
+balls.push(
+    new Ball(
+        config.ball1.x,
+        config.ball1.y,
+        config.ball1.vx,
+        config.ball1.vy,
+        config.ball1.mass,
+        null,
+        constant
+    )
+)
+balls.push(
+    new Ball(
+        config.ball2.x,
+        config.ball2.y,
+        config.ball2.vx,
+        config.ball2.vy,
+        config.ball2.mass,
+        balls[0],
+        constant
+    )
+)
 balls[0].secondBall = balls[1]
 
 function loop() {
     ctx.clearRect(0, 0, canvas.width, canvas.height)
-    for (const ball of balls) {
-        ball.update(ctx)
-    }
+    for (const ball of balls) ball.update(ctx)
     requestAnimationFrame(loop)
 }
 loop()
